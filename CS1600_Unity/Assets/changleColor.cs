d3259841227c94981e74eae77557c84ce87cd91f
kurt jones <kurtwilliamjones@gmail.com>
2017-01-26 10:47:13 -0700

    Unity Project new change Color
    change color

---
+++ CS1600_Unity/Assets/changleColor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class changleColor : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Renderer>().material.color = Color.black;
+        }
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+	}
+}
