0fe730cae4eeb6a34a1fb48e11abace60145bd60
kurt jones <kurtwilliamjones@gmail.com>
2017-04-11 17:42:58 -0600

    Calculator V0.4

--- CS1600_Unity/Assets/Scripts/Calculator.cs
+++ CS1600_Unity/Assets/Scripts/Calculator.cs
@@ -6,10 +6,15 @@ using UnityEngine.UI;
 public class Calculator : MonoBehaviour {
 
     public InputField userInput;
-    string operation = "";
-    string num1 = "";
-    string num2 = "";
-    float answer = 0f;
+    public InputField answerField;
+    public string operation = "";
+    public string num1 = "";
+    public string num2 = "";
+    public float answer = 0f;
+    public float exponentialNum1 = 0f;
+    public string symbol = "";
+    public float convertedNum1 = 0f;
+    public float convertedNum2 = 0f;
 	// Use this for initialization
 	void Start ()
     {  
@@ -20,43 +25,55 @@ public class Calculator : MonoBehaviour {
     {
         operation = "Addition";
         num1 = userInput.text;
-        float convertedNum1 = float.Parse(num1);
-
         Clear();
+        convertedNum1 = float.Parse(num1);
+        exponentialNum1 += convertedNum1;
+       
     }
-    public void Subtraction(string value)
+    public void Subtraction()
     {
         operation = "Subtraction";
         num1 = userInput.text;
+        exponentialNum1 -= float.Parse(num1);
+        Clear();
     }
-    public void Multiplication(string value)
+    public void Multiplication()
     {
         operation = "Multiplication";
         num1 = userInput.text;
         Clear();
     }
-    public void Division(string value)
+    public void Division()
     {
         operation = "Division";
-        num1 = userInput.text += value;
+        num1 = userInput.text;
         Clear();
     }
     public void Equals()
     {
         num2 = userInput.text;
         Clear();
-        float convertedNum1 = float.Parse(num1);
-        float convertedNum2 = float.Parse(num2);
+        convertedNum2 = float.Parse(num2);
         if (operation == "Addition")
         {
-           answer = convertedNum1 + convertedNum2; 
+           answer = exponentialNum1 + convertedNum2;
+            exponentialNum1 = answer;
+            symbol = "+";
+        }
+        if (operation == "Subtraction")
+        {
+            answer = exponentialNum1 - convertedNum2;
+            exponentialNum1 = answer;
         }
         string outAnswer = answer.ToString();
-        userInput.text = outAnswer;
+        answerField.text = outAnswer + symbol;
+        Clear();
     }
     public void Clear()
     {
-        userInput.text = "";  
+        userInput.text = "";
+        answerField.text = "";
+        
     }
     public void AddToInput(string value)
     {
