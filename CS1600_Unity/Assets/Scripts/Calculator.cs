2854f5ab7d83519953ac4057ebbef0725ce870b9
kurt jones <kurtwilliamjones@gmail.com>
2017-04-11 10:53:09 -0600

    calculator V0.3

--- CS1600_Unity/Assets/Scripts/Calculator.cs
+++ CS1600_Unity/Assets/Scripts/Calculator.cs
@@ -5,55 +5,63 @@ using UnityEngine.UI;
 
 public class Calculator : MonoBehaviour {
 
-    List<float> numbers = new List<float> ();
-    
     public InputField userInput;
+    string operation = "";
+    string num1 = "";
+    string num2 = "";
+    float answer = 0f;
 	// Use this for initialization
 	void Start ()
-    {
-        //take number from input box and add to list
-        
+    {  
         
 	}
-	public void GetInput()
-    {
-        string input = userInput.text;
-        float converted = float.Parse(input);
-        numbers[0] = converted;
-        
-    }
 	// create functions for each action
 	public void Addition ()
     {
-        //get numbers from input box
-        //add numbers together
-        
+        operation = "Addition";
+        num1 = userInput.text;
+        float convertedNum1 = float.Parse(num1);
 
+        Clear();
     }
-    public void Subtraction()
+    public void Subtraction(string value)
     {
-
+        operation = "Subtraction";
+        num1 = userInput.text;
     }
-    public void Multiplication()
+    public void Multiplication(string value)
     {
-
+        operation = "Multiplication";
+        num1 = userInput.text;
+        Clear();
     }
-    public void Division()
+    public void Division(string value)
     {
-
+        operation = "Division";
+        num1 = userInput.text += value;
+        Clear();
     }
     public void Equals()
     {
-
+        num2 = userInput.text;
+        Clear();
+        float convertedNum1 = float.Parse(num1);
+        float convertedNum2 = float.Parse(num2);
+        if (operation == "Addition")
+        {
+           answer = convertedNum1 + convertedNum2; 
+        }
+        string outAnswer = answer.ToString();
+        userInput.text = outAnswer;
     }
     public void Clear()
     {
-        userInput.Select();
         userInput.text = "";  
     }
     public void AddToInput(string value)
     {
         userInput.text += value;
+        
     }
     
 }
